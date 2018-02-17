5e57dc3cbe6cc8dc5bd947082f1d69a26e081eea
Rodrigo Leite <>
2018-02-17 13:59:20 -0200

    reset world configuration

--- Getnet/Scenes/ViewController.swift
+++ Getnet/Scenes/ViewController.swift
@@ -13,7 +13,11 @@ import SceneKit
 class ViewController: UIViewController {
 
     // MARK: - IBOUTLET
-    @IBOutlet weak var PlaneDectedLabel: UILabel!
+    @IBOutlet weak var planeDectedLabel: UILabel! {
+        didSet {
+            planeDectedLabel.isHidden = true
+        }
+    }
     @IBOutlet weak var sceneView: ARSCNView!
     
     // MARK: - VARIABLES
@@ -26,19 +30,47 @@ class ViewController: UIViewController {
         sceneView.session.run(configuration)
         sceneView.delegate = self
         sceneView.debugOptions = [ARSCNDebugOptions.showWorldOrigin, ARSCNDebugOptions.showFeaturePoints]
-        
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(handleTap))
+        self.sceneView.addGestureRecognizer(tapGestureRecognizer)
+    }
+    
+    // MARK: - HANDLE ACTIONS
+    @objc func handleTap(sender: UITapGestureRecognizer) {
+        guard let sceneView = sender.view as? ARSCNView else {return}
+        let touchLocation = sender.location(in: sceneView)
+        let hitTestResult = sceneView.hitTest(touchLocation, types: .existingPlaneUsingExtent)
+        if !hitTestResult.isEmpty {
+            // Add Portal
+            //self.addPortal(hitTestResult: hitTestResult.first!)
+        }
+    }
+    
+    func restartSession() {
+        sceneView.session.pause()
+        sceneView.scene.rootNode.enumerateChildNodes { (node, _) in
+            node.removeFromParentNode()
+        }
+        sceneView.session.run(configuration, options: [.resetTracking, .removeExistingAnchors])
     }
     
     
+    // MARK: - IBACTION
     @IBAction func didTouchResetButton(_ sender: UIButton) {
-        
+        restartSession()
     }
     
-
 }
 
 extension ViewController: ARSCNViewDelegate {
     
-    
+    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+        guard anchor is ARPlaneAnchor else {return}
+        DispatchQueue.main.async {
+            self.planeDetected.isHidden = false
+        }
+        DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+            self.planeDetected.isHidden = true
+        }
+    }
     
 }
