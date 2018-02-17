69510ab205e8b9754d53673bca3a13d37fc4d52b
Rodrigo Leite <>
2018-02-17 14:09:51 -0200

    update constraint and adding planes

--- Getnet/Scenes/ViewController.swift
+++ Getnet/Scenes/ViewController.swift
@@ -41,16 +41,46 @@ class ViewController: UIViewController {
         let hitTestResult = sceneView.hitTest(touchLocation, types: .existingPlaneUsingExtent)
         if !hitTestResult.isEmpty {
             // Add Portal
-            //self.addPortal(hitTestResult: hitTestResult.first!)
+            addPortal(hitTestResult: hitTestResult.first!)
         }
     }
     
+    // MARK: - LOGIC
     func restartSession() {
         sceneView.session.pause()
         sceneView.scene.rootNode.enumerateChildNodes { (node, _) in
             node.removeFromParentNode()
         }
         sceneView.session.run(configuration, options: [.resetTracking, .removeExistingAnchors])
+    }
+    
+    func addPortal(hitTestResult: ARHitTestResult) {
+        let portalScene = SCNScene(named: "Art.scnassets/Portal.scn")
+        let portalNode = portalScene!.rootNode.childNode(withName: "Portal", recursively: false)!
+        let transform = hitTestResult.worldTransform
+        let planeXposition = transform.columns.3.x
+        let planeYposition = transform.columns.3.y
+        let planeZposition = transform.columns.3.z
+        portalNode.position =  SCNVector3(planeXposition, planeYposition, planeZposition)
+        sceneView.scene.rootNode.addChildNode(portalNode)
+        addPlane(nodeName: "roof", portalNode: portalNode, imageName: "top")
+        addPlane(nodeName: "floor", portalNode: portalNode, imageName: "bottom")
+        addWalls(nodeName: "backWall", portalNode: portalNode, imageName: "back")
+        addWalls(nodeName: "sideWallA", portalNode: portalNode, imageName: "sideA")
+        addWalls(nodeName: "sideWallB", portalNode: portalNode, imageName: "sideB")
+        addWalls(nodeName: "sideDoorA", portalNode: portalNode, imageName: "sideDoorA")
+        addWalls(nodeName: "sideDoorB", portalNode: portalNode, imageName: "sideDoorB")
+    }
+    
+    func addWalls(nodeName: String, portalNode: SCNNode, imageName: String) {
+        let child = portalNode.childNode(withName: nodeName, recursively: true)
+        child?.geometry?.firstMaterial?.diffuse.contents = UIImage(named: "Art.scnassets/\(imageName).png")
+    }
+    
+    
+    func addPlane(nodeName: String, portalNode: SCNNode, imageName: String) {
+        let child = portalNode.childNode(withName: nodeName, recursively: true)
+        child?.geometry?.firstMaterial?.diffuse.contents = UIImage(named: "Art.scnassets/\(imageName).png")
     }
     
     
@@ -66,10 +96,10 @@ extension ViewController: ARSCNViewDelegate {
     func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
         guard anchor is ARPlaneAnchor else {return}
         DispatchQueue.main.async {
-            self.planeDetected.isHidden = false
+            self.planeDectedLabel.isHidden = false
         }
         DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
-            self.planeDetected.isHidden = true
+            self.planeDectedLabel.isHidden = true
         }
     }
     
